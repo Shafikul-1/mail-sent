a0a254d08e4a96246aae5afdd2a614b2a18fc5ad
Md Shafikul Islam <shafikul.18288@gmail.com>
2024-07-26 07:38:13 +0600

    queue work sample

--- app/Jobs/EmailReplyJob.php
+++ app/Jobs/EmailReplyJob.php
@@ -2,7 +2,12 @@
 
 namespace App\Jobs;
 
+use Google\Client;
+use App\Models\User;
+use Google\Service\Gmail;
+use App\Models\GoogleToken;
 use Illuminate\Bus\Queueable;
+use Google\Service\Gmail\Message;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Http\Controllers\GmailController;
@@ -15,7 +20,7 @@ class EmailReplyJob implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $datas;
-
+    private $client;
     /**
      * Create a new job instance.
      *
@@ -24,6 +29,8 @@ class EmailReplyJob implements ShouldQueue
     public function __construct($data)
     {
         $this->datas = $data;
+
+       
     }
 
     /**
@@ -33,10 +40,19 @@ class EmailReplyJob implements ShouldQueue
      */
     public function handle()
     {
+        $applicationName = "my projet";
+        $this->client = new Client();
+        $this->client->setApplicationName($applicationName);
+        $this->client->setAuthConfig(storage_path('app/client_secrate.json'));
+        $this->client->addScope('https://www.googleapis.com/auth/gmail.readonly');
+        $this->client->addScope('https://www.googleapis.com/auth/gmail.modify');
+        $this->client->addScope('https://www.googleapis.com/auth/gmail.send');
+        $this->client->setAccessType('offline');
+        $this->client->setPrompt('consent');
+        
         foreach($this->datas as $data){
             if($data->action === 'reply'){
-                $replySentController = new GmailController();
-                $replysent = $replySentController->messageSentSchedule($data->reply, $data->messageId);
+                $replysent = $this->messageSentSchedule($data->reply, $data->messageId, $data->user_id);
                 if($replysent){
                     echo "successful reply -- ";
                 } else{
@@ -44,6 +60,78 @@ class EmailReplyJob implements ShouldQueue
                 }
             }
         }
+    }
+
+    
+    public function messageSentSchedule($replyText, $messageId, $userId)
+    {
+        // $returnData = $replyText . '  ---  ' . $messageId;
+        // return $returnData;
+
+        $checking = $this->checkAccess($userId);
+        if ($checking) {
+            // Create Gmail service
+            $gmail = new Gmail($this->client);
+            try {
+                $originalMessage = $gmail->users_messages->get('me', $messageId);
+                $threadId = $originalMessage->getThreadId();
+
+                // Create the raw MIME message
+                $replyMessage = new Message();
+                $rawMessage = "From: " . $this->getHeader($originalMessage, 'From') . "\r\n";
+                $rawMessage .= "To: " . $this->getHeader($originalMessage, 'To') . "\r\n";
+                $rawMessage .= "Subject: Re: " . $this->getHeader($originalMessage, 'Subject') . "\r\n";
+                $rawMessage .= "In-Reply-To: " . $this->getHeader($originalMessage, 'Message-ID') . "\r\n";
+                $rawMessage .= "References: " . $this->getHeader($originalMessage, 'Message-ID') . "\r\n";
+                $rawMessage .= "Content-Type: text/html; charset=UTF-8\r\n";
+                $rawMessage .= "Content-Transfer-Encoding: quoted-printable\r\n";
+                $rawMessage .= "\r\n " . $replyText;
+                // return $rawMessage;
+                $rawMessageEncode = base64_encode($rawMessage);
+                $plainRawMessage = str_replace(['+', '/', '='], ['-', '_', ''], $rawMessageEncode);
+
+                $replyMessage->setRaw($plainRawMessage);
+                $replyMessage->setThreadId($threadId);
+                $sentMessage = $gmail->users_messages->send('me', $replyMessage);
+
+                return response()->json(['message' => 'Reply sent successfully']);
+            } catch (\Google\Service\Exception $e) {
+                return $e->getMessage();
+            }
+        } else {
+            return redirect()->route('home')->with('msg', "Token Expired");
+        }
+    }
+
+    private function getHeader($message, $name)
+    {
+        foreach ($message->getPayload()->getHeaders() as $header) {
+            if ($header->getName() === $name) {
+                return $header->getValue();
+            }
+        }
+        return null;
+    }
+    
+    // CHeck User Token Access ?
+    private function checkAccess($userId)
+    {
+        $dbgoogleToken = GoogleToken::where('user_id', $userId)->first();
+        if (!$dbgoogleToken) {
+            return redirect()->route('home')->with('msg', "Token is null");
+        }
+
+        $tokenData = json_decode($dbgoogleToken->access_token, true);
+        $this->client->setAccessToken($tokenData);
+
+        // Check if token has expired
+        if ($this->client->isAccessTokenExpired()) {
+            $this->client->fetchAccessTokenWithRefreshToken($this->client->getRefreshToken());
+            $newToken = json_encode($this->client->getAccessToken());
+            GoogleToken::where('user_id', $userId)->update(['access_token' => $newToken]);
+            $tokenData = $this->client->getAccessToken(); // Update tokenData after refresh
+        }
+        return true;
     }
     
 }
